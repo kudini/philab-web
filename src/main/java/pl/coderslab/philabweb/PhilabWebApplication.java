21ebb68d1e4550089b2a0fbea9f08ea957ea1b4d
Jerzy Pałka <57533018+kudini@users.noreply.github.com>
2020-08-02 17:57:45 +0200

    initial commit

---
+++ src/main/java/pl/coderslab/philabweb/PhilabWebApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.philabweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilabWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhilabWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/philabweb/PhilabWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.philabweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilabWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
